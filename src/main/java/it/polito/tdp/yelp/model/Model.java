0a1ba4788b279afbc2fe56b86dd66a48c1cde4dd
Alessio Cutaia <s262400@studenti.polito.it>
2021-06-01 13:12:49 +0200

    Punto 1b

--- src/main/java/it/polito/tdp/yelp/model/Model.java
+++ src/main/java/it/polito/tdp/yelp/model/Model.java
@@ -1,13 +1,36 @@
 package it.polito.tdp.yelp.model;
 
+import java.time.Year;
 import java.util.List;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
 import it.polito.tdp.yelp.db.YelpDao;
 
 public class Model {
 	
+	private Graph<Business, DefaultWeightedEdge> grafo;
+	private List<Business> vertici;
+	
 	public List<String> getAllCities(){
 		YelpDao dao = new YelpDao();
 		return dao.getAllCity();
 	}
+	
+	public String creaGrafo(String citta, Year anno) {
+		this.grafo = new SimpleDirectedWeightedGraph<Business, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		YelpDao dao = new YelpDao();
+		this.vertici = dao.getBusinessByCityAndYear(citta, anno);
+		Graphs.addAllVertices(this.grafo, this.vertici);
+		
+		return String.format("Grafo creato con %d vertici e %d archi\n", 
+				this.grafo.vertexSet().size(),
+				this.grafo.edgeSet().size());
+	}
+	
+	
+	
 }
